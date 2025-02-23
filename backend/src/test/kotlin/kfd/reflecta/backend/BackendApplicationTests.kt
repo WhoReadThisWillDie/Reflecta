1e46c73dd000d00341bc9c93a061bbd772c80a3f
WhoReadThisWillDie <msadkoff@mail.ru>
2025-02-23 15:21:13 +0100

    initial backend commit + database container setup

---
+++ backend/src/test/kotlin/kfd/reflecta/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package kfd.reflecta.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
